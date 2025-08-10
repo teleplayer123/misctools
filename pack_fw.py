aabe6920e1ff41e7a7e4afa50bcaa8a10cdc568b
cole sashkin <csashceh1024@gmail.com>
2025-08-09 19:42:09 -0700

    update

---
+++ pack_fw.py
@@ -0,0 +1,68 @@
+import os
+import sys
+
+class Part:
+    def __init__(self, start_offset, end_offset, name):
+        self.start_offset = start_offset
+        self.end_offset = end_offset
+        self.name = name
+
+    def align_data(self, data):
+        if len(data) < self.end_offset:
+            rem = self.end_offset - len(data)
+            data = data + bytes(rem)
+        return data
+
+    def __str__(self):
+        return \
+        f"""
+        {self.start_offset} - {self.end_offset}     {self.name}
+        """
+    
+class PackFW:
+    def __init__(self, csv_path, bin_dir, new_filename):
+        self.bin_dir = bin_dir
+        self.csv_path = csv_path
+        self.new_filename = new_filename
+
+    def parse_mtdparts(self):
+        mtdparts = {}
+        with open(self.csv_path) as fh:
+            for line in fh.readlines():
+                try:
+                    start, end, name = line.split(",")
+                    name = name.strip("\n")
+                    start = int(start, 16)
+                    end = int(end, 16)
+                except ValueError:
+                    print("Error: each line in CSV file must follow this format: int,int,str")
+                    sys.exit()
+                part_path = os.path.join(self.bin_dir, name)
+                print(f"[*] Searching for binary at location {part_path}")
+                if not os.stat(part_path):
+                    print("Error: name at the end of each line must be name of the respective binary located in bin_dir")
+                    sys.exit()
+                p = Part(start, end, name)
+                mtdparts[part_path] = p
+                print(f"[+] found partition {str(p)}")
+        return mtdparts
+    
+    def get_aligned_data(self, part, filename):
+        with open(filename, "rb") as fh:
+            data = fh.read()
+        data = part.align_data(data)
+        return data
+    
+    def write_fw(self):
+        mtdparts = self.parse_mtdparts()
+        with open(self.new_filename, "wb") as fh:
+            for path, part in mtdparts.items():
+                fh.seek(part.start_offset)
+                data = self.get_aligned_data(part, path)
+                fh.write(data)
+        print(f"[+] firmware written to file {self.new_filename}")
+                
+if __name__ == "__main__":
+    pfw = PackFW("test.csv", "test.bin")
+    pfw.parse_mtdparts()
+                
