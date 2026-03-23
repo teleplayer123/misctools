d66b97e79fbc87ff0f958327186642bb4a204909
cole sashkin <csashkin@icloud.com>
2026-03-23 15:00:49 -0700

    update

--- sec_tools/check_urls.py
+++ sec_tools/check_urls.py
@@ -10,19 +10,34 @@ URL_REGX = re.compile(r"https?://[^\s]+")
 URL_DETAILED_REGX = re.compile(r"https?://[a-zA-Z0-9\.\-_]+(?:\:[0-9]+)?(?:/[^\s]*)?")
 
 def check_file_for_urls(fp):
-    urls = []
+    """Check a file for URLs and return a tuple of (urls from text files, urls from binary files)"""
+    # urls from text files
+    urls_text = []
+    # urls from binary files
+    urls_bin = []
+
     try:
         with open(fp) as fh:
             for line in fh.readlines():
                 url = URL_REGX.findall(line)
                 if len(url) > 0:
-                    urls.extend(url)
+                    urls_text.extend(url)
+    except UnicodeDecodeError:
+        with open(fp, "rb") as fh:
+            for line in fh.readlines():
+                try:
+                    url = URL_DETAILED_REGX.findall(line.decode("utf-8"))
+                    if len(url) > 0:
+                        urls_bin.extend(url)
+                except UnicodeDecodeError:
+                    continue
     except Exception as err:
         print(f"Error processing {fp}: {err}")
-    return urls
+    return urls_text, urls_bin
 
 def main():
-    urls_by_filename = {}
+    urls_text_by_filename = {}
+    urls_bin_by_filename = {}
     usage = f"{sys.argv[0]} <dirpath>"
     parser = argparse.ArgumentParser(usage=usage)
     parser.add_argument("path", nargs=1, help="path to directory")
@@ -32,13 +47,18 @@ def main():
         for filename in filenames:
             fp = os.path.join(dirpath, filename)
             if os.path.isfile(fp):
-                urls = check_file_for_urls(fp)
-                if len(urls) > 0:
-                    urls_by_filename[fp] = urls
-    return urls_by_filename
+                urls_text, urls_bin = check_file_for_urls(fp)
+                if len(urls_text) > 0:
+                    urls_text_by_filename[fp] = urls_text
+                if len(urls_bin) > 0:
+                    urls_bin_by_filename[fp] = urls_bin
+    return urls_text_by_filename, urls_bin_by_filename
 
 if __name__ == "__main__":
-    res = main()
-    print("\nDiscovered URLs")
+    res_text, res_bin = main()
+    print("\nDiscovered URLs (Text Files)")
     print("-----------------")
-    pprint(res)
+    pprint(res_text)
+    print("\nDiscovered URLs (Binary Files)")
+    print("-----------------")
+    pprint(res_bin)
